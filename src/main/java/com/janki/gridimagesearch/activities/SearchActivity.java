cbfc60eae2ffcc19a47fa6ce5814ae4dcfb31908
Janki <janki.chauhan@gmail.com>
2015-03-25 12:13:18 -0700

    Search action bar

--- src/main/java/com/janki/gridimagesearch/activities/SearchActivity.java
+++ src/main/java/com/janki/gridimagesearch/activities/SearchActivity.java
@@ -1,10 +1,12 @@
 package com.janki.gridimagesearch.activities;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -14,6 +16,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 import android.widget.Toast;
 
 import com.janki.gridimagesearch.R;
@@ -39,11 +43,12 @@ public class SearchActivity extends ActionBarActivity {
     private ImageResultsAdapter aImageResults;
     private final int REQUEST_CODE = 20;
     private final int mMaxResults = 64;
-    String query;
-    String imageSize;
-    String imageColor;
-    String imageType;
-    String searchFilters;
+    private final String googleAPI = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8";
+    private String query;
+    private String imageSize;
+    private String imageColor;
+    private String imageType;
+    private String searchFilters = "";
     int currentPage = 0;
 
 
@@ -51,7 +56,6 @@ public class SearchActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        searchFilters = "";
         setupViews();
         imageResults = new ArrayList<ImageResult>();
         aImageResults = new ImageResultsAdapter(this, imageResults);
@@ -61,7 +65,7 @@ public class SearchActivity extends ActionBarActivity {
 
     private void setupViews() {
 
-        etQuery = (EditText) findViewById(R.id.etQuery);
+        //etQuery = (EditText) findViewById(R.id.etQuery);
         gdResults = (GridView) findViewById(R.id.gvResults);
         gdResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -85,37 +89,42 @@ public class SearchActivity extends ActionBarActivity {
                     return;
                 }
                 currentPage = (page - 1) * 8;
-                customLoadMoreDataFromApi();
+                getSearchResults(getSearchQuery());
             }
         });
 
     }
 
-    private void customLoadMoreDataFromApi() {
-
-        query = etQuery.getText().toString();
-        String searchUrl = getSearchQuery(query);
-        //Adding 'start' query parameter for loading new data
-        searchUrl = searchUrl + "&start=" + currentPage;
-        getSearchResults(searchUrl);
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_setting_activity, menu);
+        //ßMenuInflater inflater = getMenuInflater();
+        getMenuInflater().inflate(R.menu.menu_setting_activity, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String inputQuery) {
+                // perform query here
+                query = inputQuery;
+                newSearch();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             openSettings();
             return true;
@@ -149,28 +158,31 @@ public class SearchActivity extends ActionBarActivity {
             if (imageSize != null && (!imageSize.equals("none"))) {
                 searchFilters = searchFilters + "&imgsz=" + imageSize;
             }
+
+            newSearch();
         }
     }
 
-    public String getSearchQuery(String query) {
+    public String getSearchQuery() {
 
-        String searchUrl = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + query + "&rsz=8" + searchFilters;
+        String searchUrl = googleAPI
+                + "&q=" + query
+                + "&start=" + currentPage
+                + searchFilters;
 
         return searchUrl;
     }
 
-    public void onImageSearch(View view) {
+    public void newSearch() {
 
-        if (!isNetworkAvailable()) {
-            Toast.makeText(this, "No internet connection", Toast.LENGTH_SHORT).show();
-            return;
-        }
+//        if (!isNetworkAvailable()) {
+//            Toast.makeText(this, "No internet connection", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
 
-        query = etQuery.getText().toString();
-        //   Toast.makeText(this, "Search query " + query, Toast.LENGTH_SHORT).show();
-        Log.d("DEBUG", getSearchQuery(query));
+        Log.d("DEBUG", getSearchQuery());
         aImageResults.clear();
-        getSearchResults(getSearchQuery(query));
+        getSearchResults(getSearchQuery());
     }
 
     public void getSearchResults(String url) {
@@ -198,7 +210,6 @@ public class SearchActivity extends ActionBarActivity {
     private Boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        //= (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
     }
