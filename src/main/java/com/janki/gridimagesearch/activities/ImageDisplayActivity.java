ab7c79b7f5c2863a4ed11bc29103ffaa1c910b89
Janki <janki.chauhan@gmail.com>
2015-03-25 13:42:31 -0700

    Sharing Image

--- src/main/java/com/janki/gridimagesearch/activities/ImageDisplayActivity.java
+++ src/main/java/com/janki/gridimagesearch/activities/ImageDisplayActivity.java
@@ -1,35 +1,115 @@
 package com.janki.gridimagesearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+//import android.widget.ShareActionProvider;
+
+import android.support.v7.widget.ShareActionProvider;
+import android.widget.ProgressBar;
 
 import com.janki.gridimagesearch.R;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ImageDisplayActivity extends ActionBarActivity {
+
+    private ShareActionProvider miShareAction;
+    private ImageView ivImageResult;
+    private ProgressBar pbImageLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_display);
-        // Pull out the url from the intent
-
-        String url = getIntent().getStringExtra("url");
-        // Find the image view
-        ImageView ivImageResult = (ImageView) findViewById(R.id.ivFullImage);
-        // Load the image in the image view
-        Picasso.with(this).load(url).into(ivImageResult);
 
     }
 
+    public void setupShareIntent() {
+        // Fetch Bitmap Uri locally
+        ImageView ivImage = (ImageView) findViewById(R.id.ivFullImage);
+        Uri bmpUri = getLocalBitmapUri(ivImage); // see previous remote images section
+        // Create share intent as described above
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+        // Attach share event to the menu item provider
+        miShareAction.setShareIntent(shareIntent);
+    }
+
+
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable) {
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_image_display, menu);
+        MenuItem item = menu.findItem(R.id.image_share);
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        // Pull out the url from the intent
+
+        String url = getIntent().getStringExtra("url");
+        // Find the image view
+        ivImageResult = (ImageView) findViewById(R.id.ivFullImage);
+        pbImageLoad = (ProgressBar) findViewById(R.id.pbImageLoad);
+        pbImageLoad.setVisibility(View.VISIBLE);
+        // Load the image in the image view
+        Picasso.with(this)
+                .load(url)
+                .into(ivImageResult, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        pbImageLoad.setVisibility(View.GONE);
+                        setupShareIntent();
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+
+
         return true;
     }
 
